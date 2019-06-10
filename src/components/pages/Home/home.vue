5731b48b9a359713fbc9ed84c61cee1c09ea883f
jiao <123@qq.com>
2019-06-10 10:20:35 +0800

    焦二次

---
+++ src/components/pages/Home/home.vue
@@ -0,0 +1,95 @@
+<template>
+    <div id="home" ref='wrapper'>
+        <Scroll >
+        <div class= 'home-contain'>
+        <m-header></m-header>
+        <banner></banner>
+            <div class="home_1">
+                <!-- <p class="title">{{lists.name}}</p>
+                <img src="https://pic.tujia.com/upload/mobileconfig/day_190403/201904031157356821.jpg" alt="" > -->
+                <p class="title">{{list.name}}</p>
+                <img :src="list.pictureUrl" alt="" >
+                <p class="title">发现公寓</p>
+                <listfirst></listfirst>
+                
+            </div>
+        </div>
+        </Scroll>
+    </div>
+</template>
+<script>
+import Scroll  from 'base/scroll.vue'
+import Banner from  'base/banner'
+import MHeader from  'base/mheader'
+import listfirst from  'base/listfirst'
+
+export default {
+    name:'home',
+    components: {
+   MHeader,Banner,Scroll,listfirst
+    },
+    data(){
+        return{
+          lists:[],
+          list:[],
+          //live:[]
+        }
+    },
+    methods: {
+        initLists(){
+            this.$axios.post('/api/bingo/h5/portal/getPortal')
+            .then((res)=>{
+            //this.lists=res.data.topBanner.navigations[0]
+            this.list=res.data.middleBanner.navigations[0]
+            console.log(this.lists)
+            })
+        },
+    },
+      created(){
+         setTimeout(()=>{
+           this.initLists();
+           //this.initList();
+         },30)
+    
+
+  },
+  mounted() {
+        //this.initLists();
+          //this.initList();
+  },
+//   watch: {
+//       lists(){
+//         this.initLists();
+//       }
+//   },
+}
+</script>
+<style lang="less" scoped>
+@import '~style/index.less';
+#home{
+  position: fixed;
+  .top(8);
+  .bottom(0);
+  .w(375);
+  .h(500);
+  // border:1px solid red;
+  .m_t(-10);
+  .home-contain{
+    .w(375);
+      .home_1{
+        color:#fff;
+        .title{
+        color:@fc-yellow;
+        .h(40);
+        .l_h(40);
+        text-align: center;
+        font-size: @fs-xl;
+        }
+        img{
+          .h(220);
+          .w(375);
+        }
+    }
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -2,14 +2,32 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 Vue.use(Router)
-import  Recommend  from  'pages/Recommend/Recommend.vue'
+import  Home  from  'pages/Home/home.vue'
 import  Rank  from  'pages/Rank/Rank'
 export default new Router({
   mode:'history',
   routes:[
     {
-      path:'/recommend',
-      component:Recommend
+      path:'/home',
+      component:Home,
+      // children:[
+      //   {
+      //     path:'scrap',
+      //     component:Scrap
+      //   },
+      //   {
+      //     path:'handbook',
+      //     component:Handbook
+      //   },
+      //   {
+      //     path:'sale',
+      //     conponent:Sale
+      //   },
+      //   {
+      //     path:'detail',
+      //     conponent:Detail
+      //   }
+      //]
     },
     
     {
@@ -18,7 +36,7 @@ export default new Router({
     },
     {
       path:'/',
-      redirect:'/recommend' //重定向
+      redirect:'/home' //重定向
     }
   ]
 })
